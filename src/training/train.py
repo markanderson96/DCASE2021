07967b41c5bd2d5bf911f13037cb69a929a66d8a
Mark Anderson <andersm3@tcd.ie>
2021-05-27 17:48:46 +0100

    [fix] default lists to fix device allocation

--- src/training/train.py
+++ src/training/train.py
@@ -12,17 +12,26 @@ def train(model, train_loader, val_loader, num_batches_train, num_batches_val, c
     optimiser = torch.optim.SGD(model.parameters(), 
                                 lr=conf.train.lr, 
                                 momentum=conf.train.momentum)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimiser,
+                                                   gamma=conf.train.scheduler_gamma,
+                                                   step_size=conf.train.scheduler_step_size)
     epochs = conf.train.epochs
 
-    train_loss_list = np.array([])
-    train_acc_list = np.array([])
-    val_loss_list = np.array([])
-    val_acc_list = np.array([])
+    best_model_path = conf.path.best_model
+    last_model_path = conf.path.last_model
+
+    train_loss_list = []
+    train_acc_list = []
+    val_loss_list = []
+    val_acc_list = []
+    
+    best_val_acc = 0.0
+    
     model.to(device)
 
     for epoch in range(epochs + 1):
         train_iterator = iter(train_loader)
-        train_batches = tqdm(train_iterator, desc='Epoch: {}'.format(epoch))
+        train_batches = tqdm(train_iterator, desc='Epoch: {}'.format(epoch + 1))
         for batch in train_batches:
             optimiser.zero_grad()
             model.train()
@@ -31,19 +40,21 @@ def train(model, train_loader, val_loader, num_batches_train, num_batches_val, c
             Y = Y.to(device)
             
             Y_out = model(X)
-            breakpoint()
+            #breakpoint()
             train_loss, train_acc = loss_fn(Y_out, Y, conf)
-            train_loss_list.append(train_loss)
-            train_acc_list.append(train_acc)
+            train_loss_list.append(train_loss.item())
+            train_acc_list.append(train_acc.item())
             
+            train_loss.backward()
             optimiser.step()
             train_batches.update(1)
-            train_batches.set_postfix_str(s='Train Loss: {}\tTrain Acc: {}'.format(train_loss, train_acc))
+            train_batches.set_postfix_str(s='Train Loss: {:.4f}  Train Acc: {:.4f}'.format(train_loss, train_acc))
 
         avg_loss = np.mean(train_loss_list[-num_batches_train:])
         avg_acc = np.mean(train_acc_list[-num_batches_train:])
-        print('Average train loss: {}\tAverage training accuracy: {}'.format(avg_loss, avg_acc))
+        print('Average train loss: {:.4f}  Average training accuracy: {:.4f}'.format(avg_loss, avg_acc))
 
+        lr_scheduler.step()
         model.eval()
         val_iterator = iter(val_loader)
         for batch in val_iterator:
@@ -52,9 +63,19 @@ def train(model, train_loader, val_loader, num_batches_train, num_batches_val, c
             Y = Y.to(device)
             Y_out = model(X)
             val_loss, val_acc = loss_fn(Y_out, Y, conf)
-            val_loss_list.append(val_loss)
-            val_acc_list.append(val_acc)
+            val_loss_list.append(val_loss.item())
+            val_acc_list.append(val_acc.item())
 
         avg_loss = np.mean(val_loss_list[-num_batches_val:])
         avg_acc = np.mean(val_acc_list[-num_batches_val:])
-        print('Average validation loss: {}\tAverage validation accuracy: {}'.format(avg_loss, avg_acc))
+        print('Average validation loss: {:.4f}  Average validation accuracy: {:.4f}'.format(avg_loss, avg_acc))
+
+        if avg_acc > best_val_acc:
+            print("Saving the best model with valdation accuracy {:.4f}".format(avg_acc))
+            best_val_acc = avg_acc
+            best_state = model.state_dict()
+            torch.save(model.state_dict(),best_model_path)
+    
+    torch.save(model.state_dict(),last_model_path)
+
+    return best_val_acc
