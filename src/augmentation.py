7cad50f58e4b27cd5d660b9ef87f72fc968d6903
Mark Anderson <andersm3@tcd.ie>
2021-06-08 15:10:26 +0100

    added augmentation routines and corresponding calls from main

--- src/augmentation.py
+++ src/augmentation.py
@@ -9,14 +9,75 @@ import pandas as pd
 import numpy as np
 
 class Augementation():
-    def __init__(self,   
-                 conf=None) -> None:
-
+    def __init__(self, save_path, conf=None) -> None:
+        self.save_path = save_path
         self.conf = conf
-        self.time_stretch_factor = conf.features.time_stretch
-        self.direction = conf.features.direction
+
+    def frequencyMask(self, file):
+        # create copy of labels for new file
+        df = pd.read_csv(file.replace('wav', 'csv'), 
+                         header=0, 
+                         index_col=False)
+        
+        # load file and perform timeMask
+        filename = os.path.basename(file)
+        data, sr = torchaudio.load(file)
+        resample = T.Resample(sr, self.conf.features.sample_rate)
+        data = resample(data)
+        spectrogram = T.Spectrogram(
+            n_fft=self.conf.features.n_fft,
+            hop_length=self.conf.features.hop,
+            window_fn=(torch.hamming_window),
+            power=None
+        )
+        data = spectrogram(data)
+        frequency_mask = T.FrequencyMasking(freq_mask_param=80)
+        data = frequency_mask(data)
+        
+        data = F.magphase(data, power=2.0)[0]
+        
+        gl = T.GriffinLim(n_fft=self.conf.features.n_fft,
+                          hop_length=self.conf.features.hop)
+        
+        data_out = gl(data)
+        torchaudio.save(self.save_path + filename[:-4] + '_freqmask.wav', data_out, self.conf.features.sample_rate)
+
+        df['Audiofilename'] = filename[:-4] + '_freqmask.wav'
+        df.to_csv(self.save_path + filename[:-4] + '_freqmask.csv')
+
+    def timeMask(self, file): 
+        # create copy of labels for new file
+        df = pd.read_csv(file.replace('wav', 'csv'), 
+                         header=0, 
+                         index_col=False)
+        
+        # load file and perform timeMask
+        filename = os.path.basename(file)
+        data, sr = torchaudio.load(file)
+        resample = T.Resample(sr, self.conf.features.sample_rate)
+        data = resample(data)
+        spectrogram = T.Spectrogram(
+            n_fft=self.conf.features.n_fft,
+            hop_length=self.conf.features.hop,
+            window_fn=(torch.hamming_window),
+            power=None
+        )
+        data = spectrogram(data)
+        time_mask = T.TimeMasking(time_mask_param=80)
+        data = time_mask(data)
+        
+        data = F.magphase(data, power=2.0)[0]
+        
+        gl = T.GriffinLim(n_fft=self.conf.features.n_fft,
+                          hop_length=self.conf.features.hop)
+        
+        data_out = gl(data)
+        torchaudio.save(self.save_path + filename[:-4] + '_timemask.wav', data_out, self.conf.features.sample_rate)
+       
+        df['Audiofilename'] = filename[:-4] + '_timemask.wav'
+        df.to_csv(self.save_path + filename[:-4] + '_timemask.csv')
     
-    def timeStretch(self, file, save_path):
+    def timeStretch(self, file, labels):
         filename = os.path.basename(file)
         data, sr = torchaudio.load(file)
         resample = T.Resample(sr, self.conf.features.sample_rate)
@@ -31,81 +92,82 @@ class Augementation():
 
         ts_u = T.TimeStretch(hop_length=self.conf.features.hop,
                              n_freq = self.conf.features.n_fft // 2 + 1,
-                             fixed_rate = (1 + self.time_stretch_factor))
+                             fixed_rate = (1 + self.conf.features.time_stretch))
         ts_d = T.TimeStretch(hop_length=self.conf.features.hop,
                             n_freq = self.conf.features.n_fft // 2 + 1,
-                            fixed_rate = (1 - self.time_stretch_factor))
+                            fixed_rate = (1 - self.conf.features.time_stretch))
 
         gl = T.GriffinLim(n_fft=self.conf.features.n_fft,
                           hop_length=self.conf.features.hop)
         
-        if self.direction == 'up':
+        if self.conf.features.direction == 'up':
             stretched_data_up = ts_u(data)
-            stretched_data_up = F.magphase(stretched_data_up)[0]
+            stretched_data_up = F.magphase(stretched_data_up, power=2.0)[0]
             stretched_data_up = gl(stretched_data_up)
-            torchaudio.save(save_path + filename[:-4] + 'stetched_up_{}.wav'
-                            .format(1 + self.time_stretch_factor),
+            torchaudio.save(self.save_path + filename[:-4] + 'stretched_{}.wav'
+                            .format(1 + self.conf.features.time_stretch),
                             stretched_data_up,
-                            sr)
+                            self.conf.features.sample_rate)
             
-        elif self.direction == 'down':
+        elif self.conf.features.direction == 'down':
             stretched_data_down = ts_d(data)
-            stretched_data_up = F.magphase(stretched_data_down)[0]
+            stretched_data_up = F.magphase(stretched_data_down, power=2.0)[0]
             stretched_data_down = gl(stretched_data_down)
-            torchaudio.save(save_path + filename[:-4] + 'stetched_down_{}.wav'
-                            .format(1 - self.time_stretch_factor),
+            torchaudio.save(self.save_path + filename[:-4] + 'stretched_{}.wav'
+                            .format(1 - self.conf.features.time_stretch),
                             stretched_data_down,
-                            sr)
+                            self.conf.features.sample_rate)
         else:
             stretched_data_up = ts_u(data)
             stretched_data_down = ts_d(data)
-            
-            stretched_data_up = F.magphase(stretched_data_up)[0]
+            stretched_data_up = F.magphase(stretched_data_up, power=2.0)[0]
             stretched_data_up = gl(stretched_data_up)
-            torchaudio.save(save_path + filename[:-4] + 'stetched_up_{}.wav'
-                .format(1 + self.time_stretch_factor),
+            torchaudio.save(self.save_path + filename[:-4] + 'stretched_{}.wav'
+                .format(1 + self.conf.features.time_stretch),
                 stretched_data_up,
-                sr)
+                self.conf.features.sample_rate)
             
-            stretched_data_down = F.magphase(stretched_data_down)[0]
+            stretched_data_down = F.magphase(stretched_data_down, power=2.0)[0]
             stretched_data_down = gl(stretched_data_down)
-            torchaudio.save(save_path + filename[:-4] + 'stetched_down_{}.wav'
-                .format(1 - self.time_stretch_factor),
+            torchaudio.save(self.save_path + filename[:-4] + 'stretched_{}.wav'
+                .format(1 - self.conf.features.time_stretch),
                 stretched_data_down,
-                sr)
+                self.conf.features.sample_rate)
 
-    def labelAugment(self, labels, save_path):
+        self._labelAugment(labels)
+
+    def _labelAugment(self, labels):
         df = pd.read_csv(labels, header=0, index_col=False)
         
-        if self.direction == 'up':
+        if self.conf.features.direction == 'up':
             filename = df['Audiofilename']
-            filename = filename[:-4][0] + 'stetched_{}.wav'.format(1 + self.time_stretch_factor)
+            filename = filename[:-4][0] + 'stetched_{}.wav'.format(1 + self.conf.features.time_stretch)
             df['Audiofilename'] = filename[:-4]
-            df['Starttime'] = np.asarray(df['Starttime'] * (1 + self.time_stretch_factor))
-            df['Endtime'] = np.asarray(df['Endtime'] * (1 + self.time_stretch_factor))
-            df.to_csv(save_path + filename[:-4][0] + '.csv')
+            df['Starttime'] = np.asarray(df['Starttime'] * (1 + self.conf.features.time_stretch))
+            df['Endtime'] = np.asarray(df['Endtime'] * (1 + self.conf.features.time_stretch))
+            df.to_csv(self.save_path + filename[:-4][0] + '.csv')
         
-        elif self.direction == 'down':
+        elif self.conf.features.direction == 'down':
             filename = df['Audiofilename']
-            filename = filename[:-4][0] + 'stretched_{}.wav'.format(1 - self.time_stretch_factor)
+            filename = filename[:-4][0] + 'stretched_{}.wav'.format(1 - self.conf.features.time_stretch)
             df['Audiofilename'] = filename[:-4]
-            df['Starttime'] = np.asarray(df['Starttime'] * (1 - self.time_stretch_factor))
-            df['Endtime'] = np.asarray(df['Endtime'] * (1 - self.time_stretch_factor))
-            df.to_csv(save_path + filename[:-4][0] + '.csv')
+            df['Starttime'] = np.asarray(df['Starttime'] * (1 - self.conf.features.time_stretch))
+            df['Endtime'] = np.asarray(df['Endtime'] * (1 - self.conf.features.time_stretch))
+            df.to_csv(self.save_path + filename[:-4][0] + '.csv')
         
         else:
             filename = df['Audiofilename']
-            filename_up = filename[0][:-4] + 'stretched_{}.wav'.format(1 + self.time_stretch_factor)
-            filename_down = filename[0][:-4] + 'stretched_{}.wav'.format(1 - self.time_stretch_factor)
+            filename_up = filename[0][:-4] + 'stretched_{}.wav'.format(1 + self.conf.features.time_stretch)
+            filename_down = filename[0][:-4] + 'stretched_{}.wav'.format(1 - self.conf.features.time_stretch)
             original_start = np.asarray(df['Starttime'])
             original_end = df['Endtime']
             
             df['Audiofilename'] = filename_up[:-4]
-            df['Starttime'] = np.asarray(original_start * (1 + self.time_stretch_factor))
-            df['Endtime'] = np.asarray(original_end * (1 + self.time_stretch_factor))
-            df.to_csv(save_path + filename_up[:-4] + '.csv', index=None)
+            df['Starttime'] = np.asarray(original_start * (1 + self.conf.features.time_stretch))
+            df['Endtime'] = np.asarray(original_end * (1 + self.conf.features.time_stretch))
+            df.to_csv(self.save_path + filename_up[:-4] + '.csv', index=None)
 
             df['Audiofilename'] = filename_down[:-4]
-            df['Starttime'] = np.asarray(original_start * (1 - self.time_stretch_factor))
-            df['Endtime'] = np.asarray(original_end * (1 - self.time_stretch_factor))
-            df.to_csv(save_path + filename_down[:-4] + '.csv', index=None)
+            df['Starttime'] = np.asarray(original_start * (1 - self.conf.features.time_stretch))
+            df['Endtime'] = np.asarray(original_end * (1 - self.conf.features.time_stretch))
+            df.to_csv(self.save_path + filename_down[:-4] + '.csv', index=None)
--- src/main.py
+++ src/main.py
@@ -16,11 +16,11 @@ from glob import glob
 from omegaconf import DictConfig
 
 from augmentation import Augementation
-from datagenerator import *
-from features import *
+from datagenerator import Datagen
+from features import featureExtract
 from utils import EpisodicBatchSampler, euclidean_dist
 from protonet import Protonet
-from prediction import *
+from prediction import eval_prototypes
 
 
 @hydra.main(config_path='../config', config_name='config')
@@ -34,25 +34,31 @@ def main(conf: DictConfig):
     if not os.path.isdir(conf.path.eval_feat):
         os.makedirs(conf.path.eval_feat)
 
+    
+    if conf.set.augment:
+        
+        if not os.path.isdir(conf.path.data_dir + '/augmented'):
+            os.makedirs(conf.path.data_dir + '/augmented')
+        if not os.path.isdir(conf.path.train_dir + '/augmented'):
+            os.makedirs(conf.path.train_dir + '/augmented')
+        
+        save_path = conf.path.data_dir + '/augmented/'
+        
+        logger.info("### Data Augementation ###")
+        aug = Augementation(save_path=save_path, conf=conf)
+        csv_files = [file for path, _, _ in os.walk(conf.path.train_dir) 
+                        for file in glob(os.path.join(path, '*.csv')) ]
+        for file in csv_files:
+            audio_path = file.replace('csv', 'wav')
+            logger.info('=== Augmenting {} ==='.format(audio_path))
+            aug.timeStretch(audio_path, file)
+            aug.frequencyMask(audio_path)
+            aug.timeMask(audio_path)
+            
+        
+        shutil.copytree(save_path, conf.path.train_dir)
+
     if conf.set.features:
-        if conf.set.augment:
-            
-            if not os.path.isdir(conf.path.data_dir + '/augmented'):
-                os.makedirs(conf.path.data_dir + '/augmented')
-            
-            save_path = conf.path.data_dir + '/augmented/'
-            
-            logger.info("### Data Augementation ###")
-            aug = Augementation(conf=conf)
-            csv_files = [file for path, _, _ in os.walk(conf.path.train_dir) 
-                         for file in glob(os.path.join(path, '*.csv')) ]
-            for file in csv_files:
-                audio_path = file.replace('csv', 'wav')
-                logger.info('=== Augmenting {} ==='.format(audio_path))
-                #aug.timeStretch(audio_path, save_path)
-                aug.labelAugment(file, save_path)
-            
-            shutil.move(save_path, conf.path.train_dir)
 
         logger.info("### Feature Extraction ###")
         Num_extract_train, data_shape = featureExtract(conf=conf, mode="train")
